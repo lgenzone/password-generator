f3989f854383c8bcf031c3ad142a31af577412f7
lgenzone <lgenzone@icloud.com>
2022-10-24 18:04:29 -0600

    updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+var userInput = prompt("How long would you like your password to be?")
+
+var passwordLength = parseInt(userInput)
+
+    if (isNaN(passwordLength)){
+
+        alert("Please enter a numeric value")
+        return
+}
+
+if (passwordLength < 8 ||passwordLength > 128) {
+    alert("Password length must be at least 8 characters and no more than 128 characters")
+    return
+}
+
+var userWantsNumbers = confirm("Would you like to include numbers in your password?")
+var userWantsSpecialCharacters = confirm("Would you like to include special characters in your password?")
+var userWantsLowercase = confirm("Would you like to include lowercase letters in your password?")
+var userWantsUppercase = confirm("Would you like to include uppercase letters in your password?")
+
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialcharactersList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = []
+
+var allLists = [numberList]
+
+for (var i = 0; i < lowercaseList.length; i++){
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
+}
+
+if (userWantsNumbers === true) {
+    allLists.push(numberList)
+}
+
+if (userWantsSpecialCharacters === true) {
+    allLists.push(specialcharactersList)
+}
+
+if (userWantsLowercase === true) {
+    allLists.push(lowercaseList)
+}
+
+If (userWantsUppercase === true) {
+
+  
+}
 
 // Write password to the #password input
 function writePassword() {
