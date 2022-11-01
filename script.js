5112b7accabb95cd8df02d38fba2a67a11b268fc
lgenzone <lgenzone@icloud.com>
2022-10-31 22:04:22 -0600

    updated javascript
    added comments

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ function randomInt(min, max) {
     var rand = Math.random()
     return Math.floor(min * (1 - rand) + rand * max)
 }
-
-function grabRandomItem(list) {
+// grabs random item list and returns to the user
+function grabRandomItem(list) { 
     return list[randomInt(list.length)]
 }
 
 function generatePassword() {
 
-var userInput = window.prompt("How long would you like your password to be?")
+var userInput = window.prompt("How long would you like your password to be?") //asks user to input length of password
 
 var passwordLength = parseInt(userInput)
-
+// returns the following message if user enters something other than a numeric value or nothing at all
     if (isNaN(passwordLength)) {
         window.alert("Please enter a numeric value")
         return
     }   
 
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8 || passwordLength > 128) { // returns the following message to the user if they input a length outside of parameters
         window.alert("Password length must be at least 8 characters and no more than 128 characters")
         return
     } 
@@ -39,34 +39,34 @@ var userWantsSpecialCharacters = window.confirm("Would you like to include speci
 var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
 var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
 
-var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialcharactersList = ["!", "@", "#", "$", "%", "^", "&", "*"]
-var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercaseList = []
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] // all possible numbers
+var specialcharactersList = ["!", "@", "#", "$", "%", "^", "&", "*"] // all possible symbols
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] // all possible lower case letters
+var uppercaseList = [] // for loop below 
 
 var allLists = []
-
+// for loop below turns lower case letters into uppercase 
 for (var i = 0; i < lowercaseList.length; i++){
-    uppercaseList[i] = lowercaseList[i].toUpperCase()
+    uppercaseList[i] = lowercaseList[i].toUpperCase() 
 }
 
 if (userWantsNumbers === true) {
-    allLists.push(numberList)
+    allLists.push(numberList) // returns passwords containing numbers
 }
 
-if (userWantsSpecialCharacters === true) {
+if (userWantsSpecialCharacters === true) { // returns passwords containing symbols
     allLists.push(specialcharactersList)
 }
 
-if (userWantsLowercase === true) {
+if (userWantsLowercase === true) { // returns passwords containing lower case letters
     allLists.push(lowercaseList)
 }
 
-if (userWantsUppercase === true) {
+if (userWantsUppercase === true) { // returns passwords containing upper case letters
     allLists.push(uppercaseList)
 }
 
-if (allLists.length === 0) {
+if (allLists.length === 0) { //if user does not enter any credentials, default will be numbers
     allLists.push(numberList)
 }
 
@@ -83,7 +83,7 @@ for (var i = 0; i < passwordLength; i++) {
 return generatedPassword
 }
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -91,5 +91,5 @@ function writePassword() {
   passwordText.value = password;  
 }
 
-// Add event listener to generate button
+// Adds event listener to generate button
 generateBtn.addEventListener("click", writePassword);
